87269556d7b82d0e75078ec68f89bf849efd4159
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-02-13 22:41:41 +0530

    added resumo.html

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
     // res.send("Hello, Sandeep!");
     res.sendFile(__dirname + '/index.html');
 })
+app.get('/resume', (req, res) => {
+    res.sendFile(__dirname + '/resume.html');
+})
 
 app.listen(port, () => {
     console.log(`Server Running on http://localhost:${port}`);
