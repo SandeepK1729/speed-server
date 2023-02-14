31cbaaeecaaf30c0a110838d55e400f8d4c8dfea
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2023-02-14 18:35:26 +0530

    added resume.html

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     // res.send("Hello, Sandeep!");
     res.sendFile(__dirname + '/index.html');
-})
+});
 app.get('/resume', (req, res) => {
     res.sendFile(__dirname + '/resume.html');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server Running on http://localhost:${port}`);
