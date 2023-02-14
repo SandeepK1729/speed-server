b7a3bbff95e09916d37fcee8cd13263338779846
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2023-02-14 16:03:01 +0000

    .

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express();
 const port = process.env.PORT || 5050;
 
 app.use('/static', express.static('static'));
-app.uss('/templates', express.static('templates'));
+app.use('/templates', express.static('templates'));
+
 
 app.get('/', (req, res) => {
     // res.send("Hello, Sandeep!");
-    res.sendFile(templates + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.get('/resume', (req, res) => {
     res.sendFile(__dirname + '/resume.html');
