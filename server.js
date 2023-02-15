bf278fb8983cbc3f5c38bef37f5558469bd57c92
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2023-02-15 16:25:42 +0000

    added database and environ

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
@@ -14,6 +17,18 @@ app.get('/resume', (req, res) => {
     res.sendFile(__dirname + '/resume.html');
 });
 
-app.listen(port, () => {
-    console.log(`Server Running on http://localhost:${port}`);
-})
+mongoose.set('strictQuery', true);
+mongoose.connect(
+    process.env.MONGO_CONNECTION_STRING, {}, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Database Connected");
+
+            // connecting server with port
+            app.listen(port, () => {
+                console.log(`Server Running on http://localhost:${port}`);
+            });
+        }
+    }
+);
